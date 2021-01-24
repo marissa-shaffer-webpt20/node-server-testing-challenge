8ad4f867f51b650947efe0ada4a4d827e34ca676
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-24 15:30:24 -0600

    edits

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const foodRouter = require("./data/food-router")
+const foodRouter = require("./food/food-router")
 
 const server = express()
 
