f61daa9b55243fc74c48f21ac9028544d46929af
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-24 15:29:01 -0600

    finished server and index files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+const foodRouter = require("./data/food-router")
+
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+
+server.use("/food", foodRouter)
+server.get("/", (req, res) => {
+	res.json({
+		message: "Welcome to our API",
+	})
+})
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+module.exports = server
